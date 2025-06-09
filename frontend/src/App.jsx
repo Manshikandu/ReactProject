f5126e32062e54496fb76bfb738cce009071b81c
manshikandu <manshikandu@gmail.com>
2025-06-10 01:03:30 +0545

    app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 
 import './App.css'
-import Genrtor from './components/Generator'
+import Button from './components/Button'
+import Generator from './components/Generator'
+// import Genrtor from './components/Generator'
 import Hero from './components/hero'
 import Workout from './components/Workout'
 
@@ -9,8 +11,9 @@ function App() {
   return (
     <main className='min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base'>
       <Hero/>
-      <Genrtor/>
+      <Generator/>
       <Workout />
+      
       
     </main>
   )
